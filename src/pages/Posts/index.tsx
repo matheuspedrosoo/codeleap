a7e92e12ff379daf348e3fc5aa8d22dad0ac9433
Matheus Pedroso <matheuspedroso404@gmail.com>
2023-04-08 11:48:14 -0300

    feat: created get posts functionality

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -2,50 +2,37 @@ import { FormNewPost } from './components/FormNewPost'
 import { PostItem } from './components/PostItem'
 import { useAuth } from '../../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
-
 import { Container, Header, PostList } from './styles'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getPosts } from '../../redux/postsSlice'
+import { AppDispatch } from '../../redux/store'
+import { RootState } from '../../redux/store'
+
+interface Post {
+	id: number
+	username: string
+	created_datetime: Date
+	title: string
+	content: string
+}
 
 export default function Posts() {
 	const { logout } = useAuth()
 	const navigate = useNavigate()
+	const dispatch = useDispatch<AppDispatch>()
+	const dataPosts = useSelector((state: RootState) => state.posts.posts)
+	// @ts-ignore
+	const posts = dataPosts.results
+
+	useEffect(() => {
+		dispatch(getPosts())
+	}, [dispatch])
 
 	const hadleLogout = () => {
 		logout()
 		navigate('/')
 	}
-
-	const data = [
-		{
-			id: 1,
-			username: 'Vini',
-			created_datetime: '2023-01-06T12:34:56Z',
-			title: 'My First Post at CodeLeap Network!',
-			content: `
-		Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi
-						tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis
-						placerat. Duis vel nibh at velit scelerisque suscipit.
-						<br />
-						<br />
-						Duis lobortis massa imperdiet quam. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu
-						sollicitudin urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat.
-		`
-		},
-		{
-			id: 2,
-			username: 'Max',
-			created_datetime: '2023-04-06T12:34:56Z',
-			title: 'My Secound Post at CodeLeap Network!',
-			content: `
-		Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi
-						tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis
-						placerat. Duis vel nibh at velit scelerisque suscipit.
-						<br />
-						<br />
-						Duis lobortis massa imperdiet quam. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu
-						sollicitudin urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat.
-		`
-		}
-	]
 
 	return (
 		<Container>
@@ -61,8 +48,8 @@ export default function Posts() {
 
 			<FormNewPost />
 
-			{data &&
-				data.map((post) => (
+			{posts &&
+				posts.map((post: Post) => (
 					<PostList key={post.id}>
 						<PostItem post={post} />
 					</PostList>
