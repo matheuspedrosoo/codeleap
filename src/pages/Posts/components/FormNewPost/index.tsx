7079ac634cb18764402b98596ac6a22f2469eec1
Matheus Pedroso <matheuspedroso404@gmail.com>
2023-04-08 11:47:39 -0300

    feat: adjust layout and created post functionality

--- src/pages/Posts/components/FormNewPost/index.tsx
+++ src/pages/Posts/components/FormNewPost/index.tsx
@@ -1,26 +1,78 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { ButtonCustom } from '../../../../components/Button'
+import InputField from '../../../../components/InputField'
+import { useAuth } from '../../../../context/AuthContext'
+import { createPost, getPosts } from '../../../../redux/postsSlice'
+import { AppDispatch } from '../../../../redux/store'
 
 import { FormInsertContainer } from './styles'
 
+interface IPostProps {
+	title: string
+	content: string
+}
+
 export function FormNewPost() {
+	const { user } = useAuth()
+	const [post, setPost] = useState<IPostProps>({
+		title: '',
+		content: ''
+	})
+	const dispatch = useDispatch<AppDispatch>()
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		const { name, value } = e.target
+		setPost((prevPost) => ({ ...prevPost, [name]: value }))
+	}
+
+	const hadleCreateNewPost = () => {
+		const newPost = {
+			username: user,
+			title: post.title,
+			content: post.content
+		}
+
+		dispatch(createPost(newPost))
+			.then(() => {
+				dispatch(getPosts())
+				setPost({ title: '', content: '' })
+			})
+			.catch((error) => {
+				console.error('Error creating new post: ', error)
+			})
+	}
+
 	return (
 		<FormInsertContainer>
 			<span className='title'>What’s on your mind?</span>
 
-			<label htmlFor='title'>Title</label>
-			<input
-				id='title'
-				type='text'
-				placeholder='Hello world'
-			/>
+			<div>
+				<InputField
+					id='title'
+					name='title'
+					type='text'
+					placeholder='Hello world'
+					value={post.title}
+					onChange={handleInputChange}
+					label='Title'
+				/>
+				<InputField
+					id='content'
+					name='content'
+					type='textarea'
+					placeholder='Content here'
+					value={post.content}
+					onChange={handleInputChange}
+					label='Content'
+				/>
+			</div>
 
-			<label htmlFor='content'>Content</label>
-			<textarea
-				id='content'
-				placeholder='Content here'
+			<ButtonCustom
+				title='create'
+				disabled={!post.title || !post.content}
+				onClick={() => hadleCreateNewPost()}
 			/>
-
-			<ButtonCustom title='create' />
 		</FormInsertContainer>
 	)
 }
--- src/pages/Posts/components/FormNewPost/styles.ts
+++ src/pages/Posts/components/FormNewPost/styles.ts
@@ -6,57 +6,8 @@ export const FormInsertContainer = styled.div`
 	padding: 2.4rem;
 	border-radius: 1.6rem;
 	border: 0.1rem solid ${({ theme }) => theme.colors.gray60};
-
 	display: flex;
 	flex-direction: column;
-
-	label {
-		margin-top: 2.4rem;
-		font-size: 1.6rem;
-		font-weight: 400;
-		cursor: pointer;
-		color: ${({ theme }) => theme.colors.black};
-	}
-
-	input,
-	textarea {
-		margin-top: 8px;
-		height: 3.2rem;
-		padding: 8px 11.2px;
-		font-size: 1.4rem;
-		line-height: 1.6rem;
-		outline: none;
-		border: none;
-		border-radius: 0.8rem;
-		border: 0.1rem solid ${({ theme }) => theme.colors.gray};
-
-		::placeholder {
-			color: ${({ theme }) => theme.colors.gray50};
-		}
-	}
-
-	textarea {
-		height: 7.4rem;
-		resize: none;
-
-		::-webkit-scrollbar {
-			width: 10px;
-		}
-
-		::-webkit-scrollbar-thumb {
-			background-color: #888;
-			border-radius: 10px;
-		}
-
-		::-webkit-scrollbar-track {
-			background-color: #f0f0f0;
-			border-radius: 10px;
-		}
-
-		::-webkit-scrollbar-corner {
-			background-color: #f0f0f0;
-		}
-	}
 
 	@media (max-width: 768px) {
 		width: 95%;
