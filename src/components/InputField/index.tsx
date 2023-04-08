6aa379a666c6dd6789ac2b401a668b334ddd2b1c
Matheus Pedroso <matheuspedroso404@gmail.com>
2023-04-08 11:46:02 -0300

    feat: created InputField Custom

---
+++ src/components/InputField/index.tsx
@@ -0,0 +1,41 @@
+import { Container } from './styles'
+
+interface InputFieldProps {
+	id: string
+	name: string
+	type: string
+	placeholder?: string
+	value: string
+	label?: string
+	onChange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+}
+
+const InputField = ({ id, name, type, placeholder, value, onChange, label }: InputFieldProps) => {
+	const isTextArea = type === 'textarea'
+
+	return (
+		<Container>
+			<label htmlFor={id}>{label}</label>
+			{isTextArea ? (
+				<textarea
+					id={id}
+					name={name}
+					placeholder={placeholder}
+					value={value}
+					onChange={onChange}
+				/>
+			) : (
+				<input
+					id={id}
+					name={name}
+					type={type}
+					placeholder={placeholder}
+					value={value}
+					onChange={onChange}
+				/>
+			)}
+		</Container>
+	)
+}
+
+export default InputField
